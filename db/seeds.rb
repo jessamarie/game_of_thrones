e9c096ab50df7bc4f7a47bca3cac245ed7f60bc0
Jessa Marie Barre <jmlbarre@gmail.com>
2017-08-02 16:25:27 -0400

    created house, character models and relations

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,30 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+Character.destroy_all
+House.destroy_all
+
+nights_watch = House.create(name: "Night's Watch", motto: "Sword in the Darkness")
+jeor = nights_watch.characters.create(name: "Jeor Mormont")
+thorne = nights_watch.characters.create(name: "Ser Alliser Thorne")
+
+
+stark = House.create(name: "House Stark", motto: "Winter is Coming")
+catelyn = stark.characters.create(name: "Lady Catelyn Stark")
+eddard = stark.characters.create(name: "Eddard Stark")
+rob = stark.characters.create(name: "Rob Stark")
+
+
+targaryen = House.create(name: "House Targaryen", motto: "Fire and Blood")
+viserys = targaryen.characters.create(name: "Viserys Targaryen")
+daenerys = targaryen.characters.create(name: "Daenerys Targaryen")
+
+lannister = House.create(name: "House Lannister", motto: "Hear Me Roar")
+tywin = lannister.characters.create(name: "Tywin Lannister")
+tyrion = lannister.characters.create(name: "Tyrion Lannister")
+
+
+baratheon = House.create(name: "House Baratheon", motto: "Ours is the Fury")
+renly = baratheon.characters.create(name: "Renly Baratheon")
+stannis = baratheon.characters.create(name: "Stannis Baratheon")
