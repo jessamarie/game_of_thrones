0c4cc6a59a625c0d0563e93cf0bf9dc7272a5d98
Jessa Marie Barre <jmlbarre@gmail.com>
2017-08-03 12:45:43 -0400

    Added gotfaker and populated seedfile

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,34 +1,30 @@
-# This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the rails db:seed command (or created alongside the database with db:setup).
-#
-# Examples:
-#
-#   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
-#   Character.create(name: 'Luke', movie: movies.first)
+require 'gotfaker'
 
 Character.destroy_all
 House.destroy_all
 
-nights_watch = House.create(name: "Night's Watch", motto: "Sword in the Darkness")
-jeor = nights_watch.characters.create(name: "Jeor Mormont")
-thorne = nights_watch.characters.create(name: "Ser Alliser Thorne")
+character = GOTFaker::Character
+house = GOTFaker::House
+
+house1 = House.create(name: house.name, motto: "Sword in the Darkness")
+house1.characters.create(name: character.first_name +  " " + character.last_name)
+house1.characters.create(name: character.first_name + character.last_name)
 
 
-stark = House.create(name: "House Stark", motto: "Winter is Coming")
-catelyn = stark.characters.create(name: "Lady Catelyn Stark")
-eddard = stark.characters.create(name: "Eddard Stark")
-rob = stark.characters.create(name: "Rob Stark")
+house2 = House.create(name: house.name, motto: "Winter is Coming")
+house2.characters.create(name: character.first_name +  " " + character.last_name)
+house2.characters.create(name: character.random_name)
 
 
-targaryen = House.create(name: "House Targaryen", motto: "Fire and Blood")
-viserys = targaryen.characters.create(name: "Viserys Targaryen")
-daenerys = targaryen.characters.create(name: "Daenerys Targaryen")
+house3 = House.create(name: house.name, motto: "Fire and Blood")
+house3.characters.create(name: character.first_name +  " " + character.last_name)
+house3.characters.create(name: character.first_name +  " " + character.last_name)
 
-lannister = House.create(name: "House Lannister", motto: "Hear Me Roar")
-tywin = lannister.characters.create(name: "Tywin Lannister")
-tyrion = lannister.characters.create(name: "Tyrion Lannister")
+house4 = House.create(name: house.name, motto: "Hear Me Roar")
+house4.characters.create(name: character.first_name +  " " + character.last_name)
+house4.characters.create(name: character.first_name +  " " + character.last_name)
 
 
-baratheon = House.create(name: "House Baratheon", motto: "Ours is the Fury")
-renly = baratheon.characters.create(name: "Renly Baratheon")
-stannis = baratheon.characters.create(name: "Stannis Baratheon")
+house5 = House.create(name: house.name, motto: "Ours is the Fury")
+house5.characters.create(name: character.first_name +  " " + character.last_name)
+house5.characters.create(name: character.first_name +  " " + character.last_name)
